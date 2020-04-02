26c61f24c801ca156cf3ad41d7b7bc75086d5c26
Shibasis Patnaik <shibasis0801@gmail.com>
2020-04-02 19:14:42 +0530

    Returning slightly more realistic data

--- src/main/kotlin/com/pravega/backend/controllers/api/FriendController.kt
+++ src/main/kotlin/com/pravega/backend/controllers/api/FriendController.kt
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import java.util.stream.Collectors
+import kotlin.collections.HashMap
 
 @RestController
 class FriendController {
@@ -11,9 +14,12 @@ class FriendController {
     lateinit var jdbcTemplate: JdbcTemplate
 
     @GetMapping("/friends")
-    fun getAllFriends(): List<Pair<String, Int>> {
-        return jdbcTemplate.query("SELECT * FROM friends ORDER BY age desc") { result, _ ->
-            Pair(result.getString("name"), result.getInt("age"))
+    fun getAllFriends(): HashMap<String, Pair<String, Int>> {
+        // Write better Kotlin code
+        val results = hashMapOf<String, Pair<String, Int>>()
+        jdbcTemplate.query("SELECT * FROM friends ORDER BY age desc") { result, _ ->
+            results[UUID.randomUUID().toString()] = Pair(result.getString("name"), result.getInt("age"))
         }
+        return results;
     }
 }
